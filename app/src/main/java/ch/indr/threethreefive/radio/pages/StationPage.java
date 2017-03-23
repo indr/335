9ce5100b09920f38065b8e6f1bb27643a2145361
indr <mail@indr.ch>
2017-03-23 10:47:06 +0800

    Remove RadioMediaItemFactory

--- app/src/main/java/ch/indr/threethreefive/radio/pages/StationPage.java
+++ app/src/main/java/ch/indr/threethreefive/radio/pages/StationPage.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.media.MediaMetadataCompat;
 
 import com.octo.android.robospice.persistence.exception.SpiceException;
 import com.octo.android.robospice.request.listener.RequestListener;
@@ -26,14 +27,19 @@ import ch.indr.threethreefive.R;
 import ch.indr.threethreefive.commands.AddToPlaylist;
 import ch.indr.threethreefive.commands.OpenWebsite;
 import ch.indr.threethreefive.commands.PlayMedia;
+import ch.indr.threethreefive.data.network.radioBrowser.model.Station;
 import ch.indr.threethreefive.libs.Environment;
 import ch.indr.threethreefive.libs.MediaItem;
 import ch.indr.threethreefive.libs.PageItemsBuilder;
 import ch.indr.threethreefive.navigation.SpiceBasePage;
-import ch.indr.threethreefive.radio.RadioMediaItemFactory;
-import ch.indr.threethreefive.data.network.radioBrowser.StationRequest;
-import ch.indr.threethreefive.data.network.radioBrowser.model.Station;
 import ch.indr.threethreefive.services.UiModeManager;
+
+import static android.support.v4.media.MediaMetadataCompat.METADATA_KEY_DISPLAY_TITLE;
+import static android.support.v4.media.MediaMetadataCompat.METADATA_KEY_MEDIA_ID;
+import static android.support.v4.media.MediaMetadataCompat.METADATA_KEY_MEDIA_URI;
+import static android.support.v4.media.MediaMetadataCompat.METADATA_KEY_TITLE;
+import static ch.indr.threethreefive.libs.MetadataKeys.METADATA_KEY_RADIO_ID;
+import static ch.indr.threethreefive.libs.PageUris.radioStation;
 
 public class StationPage extends SpiceBasePage implements RequestListener<Station[]> {
 
@@ -44,6 +50,7 @@ public class StationPage extends SpiceBasePage implements RequestListener<Statio
   public StationPage(Environment environment) {
     super(environment);
   }
+
 
   @Override public void onCreate(@NonNull Context context, @NonNull Uri uri, Bundle bundle) {
     super.onCreate(context, uri, bundle);
@@ -74,7 +81,7 @@ public class StationPage extends SpiceBasePage implements RequestListener<Statio
 
     final PageItemsBuilder builder = pageItemsBuilder();
 
-    MediaItem mediaItem = RadioMediaItemFactory.make(station);
+    MediaItem mediaItem = makeMediaItem(station);
     builder.add(new PlayMedia(mediaItem));
     builder.add(new AddToPlaylist(mediaItem));
     builder.addToggleFavorite(getCurrentPageLink());
@@ -117,5 +124,20 @@ public class StationPage extends SpiceBasePage implements RequestListener<Statio
 
   private boolean isButtonView() {
     return uiModeManager.getCurrentUiMode() == UiModeManager.UI_MODE_BUTTONS;
+  }
+
+  private static MediaItem makeMediaItem(final @NonNull Station station) {
+    final String pageUri = radioStation(station.getId()).toString();
+    final String mediaUri = station.getUrl();
+
+    final MediaMetadataCompat metadata = new MediaMetadataCompat.Builder()
+        .putString(METADATA_KEY_DISPLAY_TITLE, station.getName())
+        .putString(METADATA_KEY_MEDIA_ID, mediaUri)
+        .putString(METADATA_KEY_MEDIA_URI, mediaUri)
+        .putString(METADATA_KEY_RADIO_ID, station.getId())
+        .putString(METADATA_KEY_TITLE, station.getName())
+        .build();
+
+    return new MediaItem(metadata, pageUri);
   }
 }
