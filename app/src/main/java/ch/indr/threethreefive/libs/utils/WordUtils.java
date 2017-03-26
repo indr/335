9dcea1b76d01ae1a40df4e66dc221b54c4aefebd
indr <mail@indr.ch>
2017-03-26 18:22:25 +0800

    Add compile.exclude for org.apache.commons

--- app/src/main/java/ch/indr/threethreefive/libs/utils/WordUtils.java
+++ app/src/main/java/ch/indr/threethreefive/libs/utils/WordUtils.java
@@ -9,8 +9,39 @@ package ch.indr.threethreefive.libs.utils;
 
 public final class WordUtils {
 
-  public static String capitalize(final String value) {
-    return org.apache.commons.lang3.text.WordUtils.capitalize(value);
+  public static String capitalize(final String str) {
+    return capitalize(str, null);
+  }
+
+  public static String capitalize(final String str, final char... delimiters) {
+    final int delimLen = delimiters == null ? -1 : delimiters.length;
+    if (StringUtils.isEmpty(str) || delimLen == 0) {
+      return str;
+    }
+    final char[] buffer = str.toCharArray();
+    boolean capitalizeNext = true;
+    for (int i = 0; i < buffer.length; i++) {
+      final char ch = buffer[i];
+      if (isDelimiter(ch, delimiters)) {
+        capitalizeNext = true;
+      } else if (capitalizeNext) {
+        buffer[i] = Character.toTitleCase(ch);
+        capitalizeNext = false;
+      }
+    }
+    return new String(buffer);
+  }
+
+  private static boolean isDelimiter(final char ch, final char[] delimiters) {
+    if (delimiters == null) {
+      return Character.isWhitespace(ch);
+    }
+    for (final char delimiter : delimiters) {
+      if (ch == delimiter) {
+        return true;
+      }
+    }
+    return false;
   }
 
 }
